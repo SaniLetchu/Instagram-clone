b56958d003a7820ec57c1cf76f5fde9ec5ef912c
Sani Letchu <sani.letchu@gmail.com>
2023-03-23 23:32:01 +0200

    fine tune login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import { Button, TextField, Grid, Typography, Box } from '@mui/material';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {
+	Button,
+	TextField,
+	Grid,
+	Typography,
+	Box,
+	ButtonBase,
+	Link,
+} from '@mui/material';
 import * as Yup from 'yup';
 
 const initialValues = {
@@ -9,7 +17,7 @@ const initialValues = {
 };
 
 const validationSchema = Yup.object().shape({
-	email: Yup.string().email().required('Email is required'),
+	email: Yup.string().email('Invalid email').required('Email is required'),
 	password: Yup.string().required('Password is required'),
 });
 
@@ -25,7 +33,7 @@ const LoginForm = () => {
 				}}
 				validationSchema={validationSchema}
 			>
-				{({ isSubmitting, isValid }) => (
+				{({ isSubmitting, errors, touched }) => (
 					<Form>
 						<Grid container direction="column" spacing={2}>
 							<Grid item display={'flex'} justifyContent={'center'} my={3}>
@@ -62,6 +70,11 @@ const LoginForm = () => {
 									variant="filled"
 									fullWidth
 								/>
+								{errors.email && touched.email && (
+									<Typography sx={{ color: 'red', textAlign: 'center' }}>
+										<ErrorMessage name="email" />
+									</Typography>
+								)}
 							</Grid>
 							<Grid item>
 								<Field
@@ -81,6 +94,11 @@ const LoginForm = () => {
 									variant="filled"
 									fullWidth
 								/>
+								{errors.password && touched.password && (
+									<Typography sx={{ color: 'red', textAlign: 'center' }}>
+										<ErrorMessage name="password" />
+									</Typography>
+								)}
 							</Grid>
 							<Grid item>
 								<Button
@@ -88,25 +106,95 @@ const LoginForm = () => {
 									variant="contained"
 									color="primary"
 									fullWidth
-									disabled={isSubmitting || !isValid}
+									disabled={isSubmitting}
 								>
-									Login
+									Log in
 								</Button>
 							</Grid>
 						</Grid>
 					</Form>
 				)}
 			</Formik>
+			<Box component="div" my={4}>
+				<Typography
+					sx={{
+						width: '100%',
+						textAlign: 'center',
+						color: 'rgb(142, 142, 142)',
+						borderBottom: '1px solid rgb(219, 219, 219)',
+						borderLeft: 'none',
+						borderRight: 'none',
+						lineHeight: '0.1em',
+						margin: '10px 0 20px',
+					}}
+					variant="body1"
+				>
+					<span style={{ backgroundColor: '#fff', padding: '0 20px' }}>OR</span>
+				</Typography>
+			</Box>
+			<Box
+				component="div"
+				my={2}
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<ButtonBase
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						gap: 1,
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="20"
+						height="20"
+						viewBox="0 0 48 48"
+					>
+						<path
+							fill="#FFC107"
+							d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+						></path>
+						<path
+							fill="#FF3D00"
+							d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+						></path>
+						<path
+							fill="#4CAF50"
+							d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+						></path>
+						<path
+							fill="#1976D2"
+							d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+						></path>
+					</svg>
+					<Typography>Log in with Google</Typography>
+				</ButtonBase>
+			</Box>
 			<Box
 				component="div"
 				sx={{
-					borderLeft: '2px solid black',
-					borderRight: '2px solid black',
-					paddingLeft: '16px',
-					paddingRight: '16px',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
 				}}
 			>
-				<Typography variant="body1">Your text here</Typography>
+				<ButtonBase
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						textAlign: 'center',
+					}}
+				>
+					Forgot password?
+				</ButtonBase>
 			</Box>
 		</>
 	);
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -4,10 +4,10 @@ import LoginForm from '../../components/LoginForm';
 
 export default function LoginPage() {
 	return (
-		<Container sx={{ display: 'flex', justifyContent: 'center' }}>
+		<Container sx={{ display: 'flex', justifyContent: 'center', p: 3 }}>
 			<Box
 				sx={{
-					width: 400,
+					width: 270,
 					px: 5,
 					py: 3,
 					border: '1px solid rgb(219, 219, 219)',
